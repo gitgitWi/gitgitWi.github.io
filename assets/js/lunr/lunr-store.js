4e2d2d752082db428b9a466a2c787bf8ea336083
ezerwi <57997672+ezerwi@users.noreply.github.com>
2020-04-03 00:36:52 +0900

    git init

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Ln 0401 koweps data mini project",
+        "excerpt":"Lecture 0401~0402 - mini project lecture 0401~0402 한국 복지패널데이터 분석 경제활동, 생활실태, 복지욕구 등 수천 개 변수 정보로 구성 foreign : 통계 파일 SPSS 로드할 수 있는 package # install.packages(\"foreign\") library(foreign) library(dplyr) ## ## Attaching package: 'dplyr' ## The following objects are masked from 'package:stats': ## ## filter, lag ##...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/LN-0401-KOWEPS-data-mini-project/",
+        "teaser": null
+      },{
+        "title": "Ln 0402 홍대입구역 apt 시세 조회 by using google maps api",
+        "excerpt":"Lecture0402_analyse APTs values around 홍대입구역 using Google Map Api Lecture0402 # analyse APTs values around 홍대입구역 using Google Map Api install ggmap &amp; import libraries library(ggplot2) theme_update(text=element_text(family=\"NanumBarunGothic\")) import csv files station &lt;- read.csv(\"~/Desktop/johnwi_KNOU/Programming/1. R/handouts/0402/역주소.csv\") head(station) ## 역명 구주소 ## 1 시청 서울 중구 서소문동 27 ## 2 을지로입구 서울 중구...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/LN-0402-%ED%99%8D%EB%8C%80%EC%9E%85%EA%B5%AC%EC%97%AD-APT-%EC%8B%9C%EC%84%B8-%EC%A1%B0%ED%9A%8C-by-using-Google-maps-API/",
+        "teaser": null
+      }]
