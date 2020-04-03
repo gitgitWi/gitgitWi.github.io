49d50b82c34f4dea2ac02a44a16ad3935a170dbf
ezerwi <57997672+ezerwi@users.noreply.github.com>
2020-04-03 12:42:20 +0900

    reset indexing

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
-        "title": "Ln 0401 koweps data mini project",
-        "excerpt":"Lecture 0401~0402 - mini project lecture 0401~0402 한국 복지패널데이터 분석 경제활동, 생활실태, 복지욕구 등 수천 개 변수 정보로 구성 foreign : 통계 파일 SPSS 로드할 수 있는 package # install.packages(\"foreign\") library(foreign) library(dplyr) ## ## Attaching package: 'dplyr' ## The following objects are masked from 'package:stats': ## ## filter, lag ##...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/LN-0401-KOWEPS-data-mini-project/",
+        "title": "Lecture 0401 analyze KOWEPS data using ggplot",
+        "excerpt":"한국 복지패널데이터 분석 경제활동, 생활실태, 복지욕구 등 수천 개 변수 정보로 구성 foreign : 통계 파일 SPSS 로드할 수 있는 package # install.packages(\"foreign\") library(foreign) library(dplyr) ## ## Attaching package: 'dplyr' ## The following objects are masked from 'package:stats': ## ## filter, lag ## The following objects are masked from 'package:base':...","categories": ["Study","R"],
+        "tags": ["R","ggplot","bigdata","LectureNotes"],
+        "url": "http://localhost:4000/study/r/LN-0401-KOWEPS-data-mini-project/",
         "teaser": null
       },{
-        "title": "Ln 0402 홍대입구역 apt 시세 조회 by using google maps api",
-        "excerpt":"Lecture0402_analyse APTs values around 홍대입구역 using Google Map Api Lecture0402 # analyse APTs values around 홍대입구역 using Google Map Api install ggmap &amp; import libraries library(ggplot2) theme_update(text=element_text(family=\"NanumBarunGothic\")) import csv files station &lt;- read.csv(\"~/Desktop/johnwi_KNOU/Programming/1. R/handouts/0402/역주소.csv\") head(station) ## 역명 구주소 ## 1 시청 서울 중구 서소문동 27 ## 2 을지로입구 서울 중구...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/LN-0402-%ED%99%8D%EB%8C%80%EC%9E%85%EA%B5%AC%EC%97%AD-APT-%EC%8B%9C%EC%84%B8-%EC%A1%B0%ED%9A%8C-by-using-Google-maps-API/",
+        "title": "Lecture 0402 analyze APTs values around 홍대입구역 using Google Maps API",
+        "excerpt":"Using Google Maps API install ggmap &amp; import libraries library(ggplot2) theme_update(text=element_text(family=\"NanumBarunGothic\")) import csv files station &lt;- read.csv(\"~/Desktop/johnwi_KNOU/Programming/1. R/handouts/0402/역주소.csv\") head(station) ## 역명 구주소 ## 1 시청 서울 중구 서소문동 27 ## 2 을지로입구 서울 중구 을지로1가 89-1 ## 3 을지로3가 서울 중구 을지로3가 70-1 ## 4 을지로4가 서울 중구 을지로4가 261-1...","categories": ["Study","R"],
+        "tags": ["R","bigdata","LectureNotes","Google API"],
+        "url": "http://localhost:4000/study/r/LN-0402-%ED%99%8D%EB%8C%80%EC%9E%85%EA%B5%AC%EC%97%AD-APT-%EC%8B%9C%EC%84%B8-%EC%A1%B0%ED%9A%8C-by-using-Google-maps-API/",
         "teaser": null
       },{
         "title": "Lecture 0403 Text Mining with R",
         "excerpt":"텍스트 마이닝 Text Mining Text-mining 개요 분석절차 형태소분석 Morphology Analsys 명사, 동사, 형용사 등 의미 지닌 품사 단어 추출 빈도표 시각화 힙합 가사 분석 import libraries and data files library(KoNLP) ## Checking user defined dictionary! library(memoise) library(dplyr) ## ## Attaching package: 'dplyr' ## The following objects are masked from 'package:stats':...","categories": ["Study"],
-        "tags": ["R","bigdata","LectureNotes","text-mining"],
+        "tags": ["R","bigdata","LectureNotes","text-mining","wordcloud"],
         "url": "http://localhost:4000/study/LN-0403-Text-mining-with-R/",
         "teaser": null
       }]
