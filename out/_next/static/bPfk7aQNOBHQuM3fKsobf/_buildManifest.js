3534f4f6d4159257689c3cf9d0bf72345a7b2c2e
gitgitWi <johnwi@knou.ac.kr>
2021-08-01 21:33:02 +0900

    FIX: deploy.yml, .gitignore 수정

--- out/_next/static/bPfk7aQNOBHQuM3fKsobf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-cfa83a47d3bbb18ae5bd.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
