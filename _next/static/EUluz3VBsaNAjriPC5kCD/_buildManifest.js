27fcce67fbe400cdce0f3e6816c4488308ea0c42
gitgitWi <gitgitWi@users.noreply.github.com>
2021-08-01 12:46:40 +0000

    Deploying to gh-pages from @ gitgitWi/gitgitWi.github.io@6caf3515773fe6d2f2d9cdf885c4f20810f046a8 🚀

---
+++ _next/static/EUluz3VBsaNAjriPC5kCD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-ab60b0f5b6aceae7fa67.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
