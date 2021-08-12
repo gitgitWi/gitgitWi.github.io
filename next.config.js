4abfcfa37eae96d2fec47f0241be3604b8c45708
gitgitWi <johnwi@knou.ac.kr>
2021-08-12 11:22:49 +0900

    Feat: MDX 설치, 테스트용 hello page 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+// @ts-check
 /* eslint-disable no-undef */
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMdx = require("@next/mdx")({
+  extension: /\.mdx$/,
+});
+
+module.exports = withMdx({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-};
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
