77d83e8cc48a5b3635bba9b0627cedd6727bf315
gitgitWi <johnwi@knou.ac.kr>
2021-08-18 09:14:18 +0900

    Feat: tsx, mdx에 TitleTexts 컴포넌트 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { MAIN_META_DESC, MAIN_TITLE } from "../constants";
+import { TitleTexts } from "../components/molecules";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>{MAIN_TITLE}</h1>
+        <TitleTexts titleText={MAIN_TITLE} />
       </main>
     </div>
   );
